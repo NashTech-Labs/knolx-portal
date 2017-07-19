cca52e4cd0296e2c17c34682e187a47d85d15a05
Ankit Kumar Barthwal <platoonhead@gmail.com>
2017-07-19 11:01:57 +0530

    Improved pagination.js code

--- public/javascripts/paginate.js
+++ public/javascripts/paginate.js
@@ -1,149 +1,73 @@
 function paginate(pageNumber, pages) {
-
     var pagination = "";
-    if (pages > 5 && pageNumber < 5) {
-        for (var iterator = 1; iterator <= 5; iterator++) {
-            if (pageNumber == iterator) {
-                pagination += "<li class='active'><a  class='paginate' id='" + iterator + "'>" + iterator + "</a></li>";
-            }
-            else {
-                pagination += "<li class='inactive'><a  class='paginate' id='" + iterator + "'>" + iterator + "</a></li>";
-            }
-
+    switch(true)
+    {
+        case (pages > 5 && pageNumber < 5) :{
+            pagination +=reducedFromIterator(1,5,pageNumber,0);
+            pagination += dots;
+            pagination += inActiveList(pages);break;
         }
-        pagination += "<li class='inactive'><a>...</a></li>";
-        pagination += "<li class='inactive'><a  class='paginate' id='" + pages + "'>" + pages + "</a></li>";
-
-    }
-
-    if (pages <= 5 && pageNumber <= 5) {
-        for (iterator = 1; iterator <= pages; iterator++) {
-            if (pageNumber == iterator) {
-                pagination += "<li class='active'><a class='paginate' id='" + iterator + "'>" + iterator + "</a></li>";
-            }
-            else {
-                pagination += "<li class='inactive'><a  class='paginate'  id='" + iterator + "'>" + iterator + "</a></li>";
-            }
+        case (pages <= 5 && pageNumber <= 5) : pagination +=reducedFromIterator(1,pages,pageNumber,0); break;
+        case (pages > 5 && pageNumber >= 5 && pages == pageNumber) : {
+            pagination += reverseIterator(5, 1, pageNumber, pages);
+            pagination += dots;
+            pagination += pageNumber == pages ? activeList(pages) : inActiveList(pages);break;
         }
-    }
-
-    if (pages > 5 && pageNumber >= 5 && pages == pageNumber) {
-        for (iterator = 5; iterator >= 1; iterator--) {
-            if (pageNumber == pages - iterator) {
-                pagination += "<li class='active'><a  class='paginate' id='" + (pages - iterator) + "'>" + (pages - iterator) + "</a></li>";
+        case (pages > 5 && pageNumber >= 5 && pageNumber < pages - 2)     : {
+            pagination += reverseIterator(2, 0, pageNumber, pageNumber);
+            for (var iterator = 1; iterator <= 2; iterator++) {
+                pagination += fromIterator(pageNumber, (pageNumber + iterator));
             }
-            else {
-                pagination += "<li class='inactive'><a  class='paginate' id='" + (pages - iterator) + "'>" + (pages - iterator) + "</a></li>";
-            }
+            pagination +=reducedFromIterator(1,2,pageNumber,pageNumber);
+            pagination += dots;
+            pagination += inActiveList(pages);
+            break;
         }
-        pagination += "<li><a>...</a></li>";
-
-        if (pageNumber == pages) {
-            pagination += "<li class='active'><a  class='paginate' id='" + pages + "'>" + pages + "</a></li>";
+        case (pages > 5 && pageNumber >= 5 && pageNumber == pages - 2)      : {
+            pagination += reverseIterator(3, 0, pageNumber, pageNumber);
+            pagination += pageNumber == (pageNumber + 1) ? activeList((pageNumber + 1)) : inActiveList((pageNumber + 1));
+            pagination += dots;
+            pagination += inActiveList(pages);
+            break;
         }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + pages + "'>" + pages + "</a></li>";
+        case (pages > 5 && pageNumber >= 5 && pageNumber > pages - 2 && pageNumber != pages) : {
+            pagination += reverseIterator(4, 0, pageNumber, pageNumber);
+            pagination += dots;
+            pagination += inActiveList(pages);
+            break;
         }
     }
-
-    if (pages > 5 && pageNumber >= 5 && pageNumber < pages - 2) {
-
-        if (pageNumber == pageNumber - 2) {
-            pagination += "<li class='active'><a  class='paginate' id='" + (pageNumber - 2) + "'>" + (pageNumber - 2) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + ( pageNumber - 2) + "'>" + (pageNumber - 2) + "</a></li>";
-        }
-
-        if (pageNumber == pageNumber - 1) {
-            pagination += "<li class='active'><a  class='paginate' id='" + (pageNumber - 1) + "'>" + (pageNumber - 1) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + (pageNumber - 1) + "'>" + (pageNumber - 1) + "</a></li>";
-        }
-        if (pageNumber == pageNumber) {
-            pagination += "<li class='active'><a  class='paginate' id='" + pageNumber + "'>" + pageNumber + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a class='paginate'  id='" + pageNumber + "'>" + pageNumber + "</a></li>";
-        }
-        if (pageNumber == pageNumber + 1) {
-            pagination += "<li class='active'><a  class='paginate' id='" + (pageNumber + 1) + "'>" + (pageNumber + 1) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + (pageNumber + 1) + "'>" + (pageNumber + 1) + "</a></li>";
-        }
-        if (pageNumber == pageNumber + 2) {
-            pagination += "<li class='active'><a class='paginate'  id='" + (pageNumber + 2) + "'>" + (pageNumber + 2) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + ( pageNumber + 2) + "'>" + (pageNumber + 2) + "</a></li>";
-        }
-
-        pagination += "<li class='inactive'><a>...</a></li>";
-        pagination += "<li class='inactive'><a  class='paginate' id='" + pages + "'>" + pages + "</a></li>";
-    }
-
-    if (pages > 5 && pageNumber >= 5 && pageNumber == pages - 2) {
-
-        if (pageNumber == pageNumber - 3) {
-            pagination += "<li class='active'><a  class='paginate' id='" + (pageNumber - 3) + "'>" + (pageNumber - 3) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + ( pageNumber - 3) + "'>" + (pageNumber - 3) + "</a></li>";
-        }
-
-        if (pageNumber == pageNumber - 2) {
-            pagination += "<li class='active'><a class='paginate'  id='" + (pageNumber - 2) + "'>" + (pageNumber - 2) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + ( pageNumber - 2) + "'>" + (pageNumber - 2) + "</a></li>";
-        }
-
-        if (pageNumber == pageNumber - 1) {
-            pagination += "<li class='active'><a  class='paginate' id='" + (pageNumber - 1) + "'>" + (pageNumber - 1) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + (pageNumber - 1) + "'>" + (pageNumber - 1) + "</a></li>";
-        }
-        if (pageNumber == pageNumber) {
-            pagination += "<li class='active'><a  class='paginate' id='" + pageNumber + "'>" + pageNumber + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + pageNumber + "'>" + pageNumber + "</a></li>";
-        }
-        if (pageNumber == pageNumber + 1) {
-            pagination += "<li class='active'><a class='paginate'  id='" + (pageNumber + 1) + "'>" + (pageNumber + 1) + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a class='paginate'  id='" + (pageNumber + 1) + "'>" + (pageNumber + 1) + "</a></li>";
-        }
-
-        pagination += "<li class='inactive'><a>...</a></li>";
-        pagination += "<li class='inactive'><a  class='paginate' id='" + pages + "'>" + pages + "</a></li>";
-    }
-
-    if (pages > 5 && pageNumber >= 5 && pageNumber > pages - 2 && pageNumber != pages) {
-
-        for (iterator = 4; iterator >= 1; iterator--) {
-
-            if (pageNumber == pageNumber - iterator) {
-                pagination += "<li class='active'><a  class='paginate' id='" + (pageNumber - iterator) + "'>" + (pageNumber - iterator) + "</a></li>";
-            }
-            else {
-                pagination += "<li class='inactive'><a class='paginate'  id='" + (pageNumber - iterator) + "'>" + (pageNumber - iterator) + "</a></li>";
-            }
-        }
-        if (pageNumber == pageNumber) {
-            pagination += "<li class='active'><a  class='paginate' id='" + pageNumber + "'>" + pageNumber + "</a></li>";
-        }
-        else {
-            pagination += "<li class='inactive'><a  class='paginate' id='" + pageNumber + "'>" + pageNumber + "</a></li>";
-        }
-
-        pagination += "<li class='inactive'><a>...</a></li>";
-        pagination += "<li class='inactive'><a  class='paginate' id='" + pages + "'>" + pages + "</a></li>";
-    }
-
     $('.pagination').html(pagination);
+}
+
+function activeList(value) {
+    return "<li class='active'><a  class='paginate' id='" + value + "'>" + value + "</a></li>"
+}
+function inActiveList(value) {
+    return "<li class='inactive'><a  class='paginate' id='" + value + "'>" + value + "</a></li>"
+}
+function dots() {
+    return "<li class='inactive'><a>...</a></li>";
+}
+
+function reverseIterator(from, till, pageNumber, compareWith){
+    var pagination = "";
+    for (var iterator = from; iterator >= till; iterator--) {
+      pagination += pageNumber == (compareWith-iterator) ? activeList((compareWith-iterator)) : inActiveList((compareWith-iterator));
+    }
+    return pagination;
+}
+
+function fromIterator(pageNumber, compareWith){
+    var pagination = "";
+    pagination += pageNumber == compareWith? activeList(compareWith) : inActiveList(compareWith);
+    return pagination;
+}
+
+function reducedFromIterator(from,till,pageNumber,offset){
+    var pagination = "";
+    for (var iterator = from; iterator <= till; iterator++) {
+        pagination += fromIterator(pageNumber, (offset+iterator));
+    }
+    return pagination;
 }

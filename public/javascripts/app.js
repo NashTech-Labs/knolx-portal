fbb740b50986e61b13f5b8ab8190aa79265e11d8
Aakash06 <aakash.jain@knoldus.in>
2017-10-11 09:56:48 +0530

    Improved UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'assets/javascripts/particles.json', function() {
+particlesJS.load('particles-js', '/assets/javascripts/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });

2a659ebf1dff8a59f1a7df3a6b979aaa250954a5
Aakash06 <aakash.jain@knoldus.in>
2017-11-07 14:32:12 +0530

    Change navbar

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,9 +1,23 @@
-
+/*
 $(window).scroll(function () {
     if ($(document).scrollTop() > 40) {
         $('nav').addClass('shrink');
     } else {
         $('nav').removeClass('shrink');
     }
-});
+});*/
+$(document).ready(function () {
+ $('#sidebarCollapse').on('click', function () {
+     $('#sidebar').toggleClass('active');
+ });
 
+ function header(){
+    var width= $(window).width();
+
+    if (width<=768) {
+       $('#sidebar').addClass('active');
+    }
+ }
+
+ header();
+});

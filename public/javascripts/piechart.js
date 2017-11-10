5bf19319c2678680558954ab49bf074a02a3013e
sidharth <sidharth.khattri@gmail.com>
2017-11-10 12:10:56 +0530

    reformatted js file

--- public/javascripts/piechart.js
+++ public/javascripts/piechart.js
@@ -1,32 +1,29 @@
-$(function ()  {
+$(function () {
+    var startDate = moment().subtract(3, 'months').startOf('day').format('YYYY-MM-DD HH:mm').toString();
+    var endDate = moment().endOf('day').format('YYYY-MM-DD HH:mm ').toString();
 
-var startDate = moment().subtract(3, 'months').startOf('day').format('YYYY-MM-DD HH:mm').toString();
-var endDate = moment().endOf('day').format('YYYY-MM-DD HH:mm ').toString();
-
-    analysis(startDate,endDate);
+    analysis(startDate, endDate);
 
     $('#demo').daterangepicker({
-        "startDate": moment().subtract(1,'months'),
+        "startDate": moment().subtract(1, 'months'),
         "endDate": moment().format('DD-MM-YYYY')
-
-    }, function(start, end, label) {
+    }, function (start, end, label) {
         var startDate = start.format('YYYY-MM-DD h:mm A');
         var endDate = end.format('YYYY-MM-DD h:mm A');
-        console.log("New date range selected: " + startDate + ' to ' + endDate + ' (predefined range: ' + label);
+
         analysis(startDate, endDate);
     });
 
 });
 
-
 function analysis(startDate, EndDate) {
-            var formData = new FormData();
-            formData.append("startDate", startDate);
-            formData.append("endDate", EndDate);
+    var formData = new FormData();
+    formData.append("startDate", startDate);
+    formData.append("endDate", EndDate);
 
-        jsRoutes.controllers.KnolxAnalysisController.pieChart().ajax(
+    jsRoutes.controllers.KnolxAnalysisController.pieChart().ajax(
         {
-           type: 'POST',
+            type: 'POST',
             processData: false,
             contentType: false,
             data: formData,
@@ -34,147 +31,137 @@ function analysis(startDate, EndDate) {
                 var csrfToken = document.getElementById('csrfToken').value;
                 return request.setRequestHeader('CSRF-Token', csrfToken);
             },
-            success: function (data){
-            var values = JSON.parse(data);
+            success: function (data) {
+                var values = JSON.parse(data);
 
-            console.log(values);
+                pieChart(values);
 
-              pieChart(values);
+                columnChart(values[1]);
 
-              columnChart(values[1]);
+                lineGraph(values[2]);
 
-              lineGraph(values[2]);
-
-              console.log("aassdfhhvh")
-           }, error: function (er) {
+            }, error: function (er) {
             console.log("No session found!");
         }
-    })
+        })
 }
 
 function columnChart(data) {
-        console.log("------->"+data);
-        var subCategoryData = [];
-        var columnGraphXAxis = [];
-        for(var i =0; i < data.length; i++) {
+    var subCategoryData = [];
+    var columnGraphXAxis = [];
+
+    for (var i = 0; i < data.length; i++) {
         var dataSub = data[i].subCategoryName;
         var sessionSub = data[i].totalSessionSubCategory;
 
         subCategoryData.push(dataSub);
         columnGraphXAxis.push(sessionSub);
+    }
 
-        }
+    var columnGraph = Highcharts.chart('column-graph', {
+        title: {
+            text: 'Knolx Session Sub-Category Analysis'
+        },
 
-        var columnGraph = Highcharts.chart('column-graph', {
+        subtitle: {
+            text: 'Plain'
+        },
 
-       title: {
-           text: 'Knolx Session Sub-Category Analysis'
-       },
+        xAxis: {
+            categories: subCategoryData
+        },
 
-       subtitle: {
-           text: 'Plain'
-       },
-
-       xAxis: {
-           categories: subCategoryData
-       },
-
-       series: [{
-           type: 'column',
-           colorByPoint: true,
-           data: columnGraphXAxis,
-           showInLegend: false
-       }]
-   });
+        series: [{
+            type: 'column',
+            colorByPoint: true,
+            data: columnGraphXAxis,
+            showInLegend: false
+        }]
+    });
 
 }
 
 function pieChart(values) {
-
-    console.log(values[0]['categoryInformation']);
     var items = [];
     var series = [];
 
     var categoryInfo = values[0]['categoryInformation'];
 
-
     for (var i = 0; i < categoryInfo.length; i++) {
         var dataSubCategory = [];
         var item = {
             "name": categoryInfo[i].categoryName,
-            "y" :   parseFloat(categoryInfo[i].totalSessionCategory/values[0].totalSession),
+            "y": parseFloat(categoryInfo[i].totalSessionCategory / values[0].totalSession),
             "drilldown": categoryInfo[i].categoryName
         };
         items.push(item);
 
-        for(var j = 0; j < categoryInfo[i]["subCategoryInfo"].length; j++ ){
-            var dataSub = [categoryInfo[i]["subCategoryInfo"][j].subCategoryName,categoryInfo[i]["subCategoryInfo"][j].totalSessionSubCategory ];
+        for (var j = 0; j < categoryInfo[i]["subCategoryInfo"].length; j++) {
+            var dataSub = [categoryInfo[i]["subCategoryInfo"][j].subCategoryName, categoryInfo[i]["subCategoryInfo"][j].totalSessionSubCategory];
             dataSubCategory.push(dataSub);
         }
-    var drillData = {
-        "name": categoryInfo[i].categoryName ,
-        "id": categoryInfo[i].categoryName,
-        "data" : dataSubCategory
-    };
+        var drillData = {
+            "name": categoryInfo[i].categoryName,
+            "id": categoryInfo[i].categoryName,
+            "data": dataSubCategory
+        };
         series.push(drillData);
     }
 
     var column = Highcharts.chart('pie-chart', {
-             chart: {
-             type: 'pie'
-         },
-             title: {
-                 text: 'Knolx Session Analysis'
-             },
-             plotOptions: {
-                 pie: {
-                     allowPointSelect: true,
-                     cursor: 'pointer',
-                     depth: 35,
-                     dataLabels: {
-                         enabled: false
-                     },
-                     showInLegend: true
-                 }
-             },
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: 'Knolx Session Analysis'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
 
-             tooltip: {
-                 headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-                 pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.percentage:.1f}%</b> of total<br/>'
-             },
-             series: [{
-                 name: 'Primary Category',
-                 colorByPoint: true,
-                 data: items
-             }],
-             drilldown: {
-                 series: series
-             },
-             responsive: {
-               rules: [{
-                 condition: {
-                   maxWidth: 500
-                 },
-                 chartOptions: {
-                   legend: {
-                     enabled: false
-                   }
-                 }
-               }]
-             }
-         });
-
+        tooltip: {
+            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.percentage:.1f}%</b> of total<br/>'
+        },
+        series: [{
+            name: 'Primary Category',
+            colorByPoint: true,
+            data: items
+        }],
+        drilldown: {
+            series: series
+        },
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        enabled: false
+                    }
+                }
+            }]
+        }
+    });
 }
 
 function lineGraph(data) {
-
     var seriesData = [];
     var xAxisData = [];
 
-    for (var i= 0; i < data.length; i++ ) {
+    for (var i = 0; i < data.length; i++) {
 
-    xAxisData.push(data[i].monthName);
-    seriesData.push(data[i].total);
+        xAxisData.push(data[i].monthName);
+        seriesData.push(data[i].total);
     }
 
     Highcharts.chart('line-graph', {
@@ -193,7 +180,7 @@ function lineGraph(data) {
             },
             labels: {
                 formatter: function () {
-                    return this.value ;
+                    return this.value;
                 }
             }
         },

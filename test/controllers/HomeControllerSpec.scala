5d0a5ea103a0d5219570282742c96fcb9f12f502
sidharth <sidharth.khattri@gmail.com>
2017-11-21 15:36:26 +0530

    removed dependency of play fake app from home controller

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,27 +1,35 @@
 package controllers
 
-import models.UsersRepository
+import com.typesafe.config.ConfigFactory
+import models.{SessionsRepository, FeedbackFormsResponseRepository, FeedbackFormsRepository, UsersRepository}
 import org.specs2.execute.{AsResult, Result}
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Around
 import org.specs2.specification.Scope
-import play.api.Application
+import play.api.{Configuration, Application}
+import play.api.libs.mailer.MailerClient
 import play.api.mvc._
 import play.api.test.{FakeRequest, PlaySpecification}
+import utilities.DateTimeUtility
 
-class HomeControllerSpec extends PlaySpecification with Results {
+class HomeControllerSpec extends PlaySpecification with Mockito {
 
-  abstract class WithTestApplication extends Around with Scope with TestEnvironment {
-    lazy val app: Application = fakeApp()
-    lazy val controller = new HomeController(knolxControllerComponent)
+  trait TestScope extends Scope {
+    val mailerClient = mock[MailerClient]
+    val feedbackFormsRepository = mock[FeedbackFormsRepository]
+    val feedbackFormsResponseRepository = mock[FeedbackFormsResponseRepository]
+    val dateTimeUtility = mock[DateTimeUtility]
+    val sessionsRepository = mock[SessionsRepository]
+    val usersRepository = mock[UsersRepository]
 
-    override def around[T: AsResult](t: => T): Result = {
-      TestHelpers.running(app)(AsResult.effectively(t))
-    }
+    val config = Configuration(ConfigFactory.load("application.conf"))
+
+    lazy val controller = new HomeController(TestHelpers.stubControllerComponents(usersRepository, config))
   }
 
   "HomeController" should {
 
-    "redirect index page to sessions page" in new WithTestApplication {
+    "redirect index page to sessions page" in new TestScope {
       val result = controller.index(FakeRequest())
 
       status(result) must be equalTo SEE_OTHER

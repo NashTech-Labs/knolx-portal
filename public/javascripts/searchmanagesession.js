956fc62ddfb9b5dd6885c4a0e58cb48e96f4d30c
Aakash06 <aakash.jain@knoldus.in>
2017-11-30 16:46:09 +0530

    Change click here for not avaiable content

--- public/javascripts/searchmanagesession.js
+++ public/javascripts/searchmanagesession.js
@@ -93,15 +93,15 @@ function slide(keyword, pageNumber, pageSize) {
                             if (sessions[session].contentAvailable) {
                                 usersFound += "<td  title='Click here for slides & videos' class='clickable-row'>" +
                                     "<a href='" + jsRoutes.controllers.SessionsController.shareContent(sessions[session].id)['url'] +
-                                    "' style='text-decoration: none;'><span class='label more-detail-session'>Click here</span></a></td>";
+                                    "' style='text-decoration: none;'><span class='label more-detail-session'>Click here</span></a>";
                             } else {
-                                usersFound += "<td><span class='label label-danger'>Not Available</span></td>";
+                                usersFound += "<td><span class='label label-danger'>Not Available</span>";
                             }
                         } else if(sessions[session].cancelled) {
-                            usersFound += "<td title='The session has been cancelled'><span class='label label-warning cancelled-session'>Cancelled</span></td>";
+                            usersFound += "<td title='The session has been cancelled'><span class='label label-warning cancelled-session'>Cancelled</span>";
                         }
                         else if(!sessions[session].completed) {
-                            usersFound += "<td title='Wait for session to be completed'><span class='label label-warning'>Pending</span></td>";
+                            usersFound += "<td title='Wait for session to be completed'><span class='label label-warning'>Pending</span>";
                         }
                             usersFound += "</td></tr>"
                     }

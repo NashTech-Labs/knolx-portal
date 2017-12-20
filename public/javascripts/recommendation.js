9767c73ee2ed4c97875382d21bd10305c2f53450
Aakash06 <aakash.jain@knoldus.in>
2017-12-20 12:42:58 +0530

    Change Javascripts fucntions

--- public/javascripts/recommendation.js
+++ public/javascripts/recommendation.js
@@ -19,24 +19,145 @@ function Recommendation() {
     });
 
     $('body').on("click", '#add-recommend-button', function () {
-        console.log("aaaaaaa");
         var text = $('.popover-content #recommend-text').val();
-        console.log("-------->" + text);
         addRecommend(text);
     });
 
 
     var self = this;
     self.recommendation = ko.observableArray([]);
-    self.userRecommendationList = ko.observableArray([]);
 
-    self.approvedOrDecline = function (id, checked) {
-        if (checked) {
-            approve(id);
-        } else {
-            decline(id);
-        }
+    self.upVoteByUser = function (id) {
+        console.log("idddd" + id);
+
+        jsRoutes.controllers.RecommendationController.upVote(id).ajax(
+            {
+                type: "POST",
+                processData: false,
+                beforeSend: function (request) {
+                    var csrfToken = document.getElementById('csrfToken').value;
+
+                    return request.setRequestHeader('CSRF-Token', csrfToken);
+                },
+                success: function (values) {
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            }
+        )
     };
+
+    self.downVoteByUser = function (id) {
+        console.log("idddd111" + id);
+        jsRoutes.controllers.RecommendationController.downVote(id).ajax(
+            {
+                type: "POST",
+                processData: false,
+                beforeSend: function (request) {
+                    var csrfToken = document.getElementById('csrfToken').value;
+
+                    return request.setRequestHeader('CSRF-Token', csrfToken);
+                },
+                success: function (values) {
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            }
+        )
+    };
+
+    self.pendingByAdmin = function (id) {
+        jsRoutes.controllers.RecommendationController.pendingRecommendation(id).ajax(
+            {
+                type: "POST",
+                processData: false,
+                beforeSend: function (request) {
+                    var csrfToken = document.getElementById('csrfToken').value;
+
+                    return request.setRequestHeader('CSRF-Token', csrfToken);
+                },
+                success: function (values) {
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            }
+        )
+    };
+
+    self.doneByAdmin = function (id) {
+        jsRoutes.controllers.RecommendationController.doneRecommendation(id).ajax(
+            {
+                type: "POST",
+                processData: false,
+                beforeSend: function (request) {
+                    var csrfToken = document.getElementById('csrfToken').value;
+
+                    return request.setRequestHeader('CSRF-Token', csrfToken);
+                },
+                success: function (values) {
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            }
+        )
+    };
+
+
+    self.declineByAdmin = function (id) {
+        jsRoutes.controllers.RecommendationController.declineRecommendation(id).ajax(
+            {
+                type: "POST",
+                processData: false,
+                beforeSend: function (request) {
+                    var csrfToken = document.getElementById('csrfToken').value;
+
+                    return request.setRequestHeader('CSRF-Token', csrfToken);
+                },
+                success: function (values) {
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            }
+        )
+    };
+
+
+    self.approveByAdmin = function (id) {
+        jsRoutes.controllers.RecommendationController.approveRecommendation(id).ajax(
+            {
+                type: "POST",
+                processData: false,
+                beforeSend: function (request) {
+                    var csrfToken = document.getElementById('csrfToken').value;
+
+                    return request.setRequestHeader('CSRF-Token', csrfToken);
+                },
+                success: function (values) {
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
+                },
+                error: function (er) {
+                    console.log(er);
+                }
+            }
+        )
+    };
+
 
     function FetchRecommendationList(pageNumber, filter) {
 
@@ -60,46 +181,6 @@ function Recommendation() {
         )
     }
 
-    function approve(id) {
-        jsRoutes.controllers.RecommendationController.approveRecommendation(id).ajax(
-            {
-                type: "POST",
-                processData: false,
-                beforeSend: function (request) {
-                    var csrfToken = document.getElementById('csrfToken').value;
-
-                    return request.setRequestHeader('CSRF-Token', csrfToken);
-                },
-                success: function (values) {
-                    FetchRecommendationList();
-                },
-                error: function (er) {
-                    console.log(er);
-                }
-            }
-        )
-    }
-
-    function decline(id) {
-        jsRoutes.controllers.RecommendationController.declineRecommendation(id).ajax(
-            {
-                type: "POST",
-                processData: false,
-                beforeSend: function (request) {
-                    var csrfToken = document.getElementById('csrfToken').value;
-
-                    return request.setRequestHeader('CSRF-Token', csrfToken);
-                },
-                success: function (values) {
-                    FetchRecommendationList();
-                },
-                error: function (er) {
-                    console.log(er);
-                }
-            }
-        )
-    }
-
     function addRecommend(text) {
         jsRoutes.controllers.RecommendationController.addRecommendation(text).ajax(
             {
@@ -112,7 +193,8 @@ function Recommendation() {
                 },
                 success: function (values) {
                     $('#add-button').popover('hide');
-                    FetchRecommendationList(1, "all");
+                    var filter = $('input[name="user-recommend-filter"]:checked').val();
+                    FetchRecommendationList(1, filter);
                 },
                 error: function (er) {
                     console.log(er);

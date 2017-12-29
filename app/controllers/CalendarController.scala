de291ca5d37fe2d80d3b37ac8858d368b0b6316a
Akshansh Jain <akshansh.jain@knoldus.in>
2017-12-29 09:56:57 +0530

    1. Added calendar UI. 2. Got data from controller for specified date range. 3. Showed open bookings where slots are available. 4. Made the events clickable.

--- app/controllers/CalendarController.scala
+++ app/controllers/CalendarController.scala
@@ -1,15 +1,20 @@
 package controllers
 
+import java.util.Date
 import javax.inject.{Inject, Named, Singleton}
 
 import akka.actor.ActorRef
 import models._
-import play.api.Configuration
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText, number, optional}
+import play.api.{Configuration, Logger}
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.Action
+import play.api.libs.json.{Json, OFormat}
+import play.api.mvc.{Action, AnyContent}
 import utilities.DateTimeUtility
 
-// this is not an unused import contrary to what intellij suggests, do not optimize
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class CalendarController @Inject()(messagesApi: MessagesApi,
@@ -22,7 +27,34 @@ class CalendarController @Inject()(messagesApi: MessagesApi,
                                    @Named("EmailManager") emailManager: ActorRef
                                   ) extends KnolxAbstractController(controllerComponents) with I18nSupport {
 
-  def renderCalendarPage = action { implicit request =>
+  implicit val knolxSessionInfoFormat: OFormat[KnolxSession] = Json.format[KnolxSession]
+
+  def renderCalendarPage: Action[AnyContent] = action { implicit request =>
     Ok(views.html.calendar.calendar())
+  }
+
+  def calendarSessions(startDate: Long, endDate: Long): Action[AnyContent] = action.async { implicit request =>
+    sessionsRepository
+      .getSessionInMonth(startDate, endDate)
+      .map { sessionInfo =>
+        val knolxSessions = sessionInfo map { session =>
+          val contentAvailable = session.youtubeURL.isDefined || session.slideShareURL.isDefined
+          KnolxSession(session._id.stringify,
+            session.userId,
+            new Date(session.date.value),
+            session.session,
+            session.topic,
+            session.email,
+            session.meetup,
+            session.cancelled,
+            "",
+            dateString = new Date(session.date.value).toString,
+            completed = new Date(session.date.value).before(new java.util.Date(dateTimeUtility.nowMillis)),
+            expired = new Date(session.expirationDate.value)
+              .before(new java.util.Date(dateTimeUtility.nowMillis)),
+            contentAvailable = contentAvailable)
+        }
+        Ok(Json.toJson(knolxSessions))
+      }
   }
 }
